3cef2fa1c08bb7c0924be80f4b92f84baade6861
jkes921003 <921003jun@gmail.com>
2024-11-15 11:12:31 +0800

    0

--- Myhtml/server.js
+++ Myhtml/server.js
@@ -5,7 +5,8 @@ var server = express();
 
 //web root
 server.use(express.static(__dirname+"/Myhtml"));
-
+server.get("/profolio", (req,res)=>{
+           res.send("Profolio");})
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
 })
